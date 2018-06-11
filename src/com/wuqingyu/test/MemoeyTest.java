9d8f59364bcb04e4a2606bef760a536ab512b1d0
WUQINGYU520 <40102719+WUQINGYU520@users.noreply.github.com>
2018-06-11 17:46:55 +0800

    Update MemoeyTest.java

--- src/com/wuqingyu/test/MemoeyTest.java
+++ src/com/wuqingyu/test/MemoeyTest.java
@@ -2,10 +2,11 @@ package com.wuqingyu.test;
 
 public class MemoeyTest {
 	public static void main(String[] args) {
-		String str1 = new StringBuilder("¼ÆËã»ú").append("Èí¼þ").toString();
+		String str1 = new StringBuilder("Â¼Ã†Ã‹Ã£Â»Ãº").append("ÃˆÃ­Â¼Ã¾").toString();
 		System.out.println(str1.intern()==str1);
 		String str2 = new StringBuilder("ja").append("va").toString();
 		System.out.println(str2.intern()==str2);
+		System.out.println("ä½ å¥½å•Š");
 	}
 
 }
