4865c2fe48e914caf68608abbdc350f35682258d
WUQINGYU <1149605076@qq.com>
2018-06-11 17:47:45 +0800

    这是冲突解决测试

--- src/com/wuqingyu/test/MemoeyTest.java
+++ src/com/wuqingyu/test/MemoeyTest.java
@@ -6,6 +6,7 @@ public class MemoeyTest {
 		System.out.println(str1.intern()==str1);
 		String str2 = new StringBuilder("ja").append("va").toString();
 		System.out.println(str2.intern()==str2);
+		System.out.println("ÄãºÃ°¡Frist");
 	}
 
 }
